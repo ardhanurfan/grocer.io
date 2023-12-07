1854f097ec59ec195f43ae9f8b918b6b24fa5787
Ardhan Nur Urfan <88726616+ardhanurfan@users.noreply.github.com>
2023-12-08 02:41:04 +0700

    feat: cart empty

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -27,80 +27,97 @@ function Cart() {
       <h1 className="text-[40px] font-bold text-purple-primary text-center">
         Your Cart
       </h1>
-      <div className="flex flex-col justify-center gap-5 px-64 py-10">
-        {cartContext?.cart.map((product: CartItem, idx: number) => {
-          return (
-            <div
-              key={idx}
-              className="w-full bg-white shadow-lg rounded-lg p-3 flex justify-between"
-            >
-              <div className="flex gap-6">
-                <img
-                  className="w-32 h-32 object-cover rounded-md"
-                  title={product.product.name}
-                  src={"/banner.jpg"}
-                ></img>
-                <div className="flex flex-col justify-center gap-2">
-                  <h3 className="text-lg font-bold">{product.product.name}</h3>
-                  <p className="text-blue-800">{product.product.jenis}</p>
-                  <p className="text-red-600 font-bold">
-                    {formatRp(product.product.price)}
+      {cartContext!.cart.length > 0 ? (
+        <div className="flex flex-col justify-center gap-5 px-64 py-10">
+          {cartContext?.cart.map((product: CartItem, idx: number) => {
+            return (
+              <div
+                key={idx}
+                className="w-full bg-white shadow-lg rounded-lg p-3 flex justify-between"
+              >
+                <div className="flex gap-6">
+                  <img
+                    className="w-32 h-32 object-cover rounded-md"
+                    title={product.product.name}
+                    src={"/banner.jpg"}
+                  ></img>
+                  <div className="flex flex-col justify-center gap-2">
+                    <h3 className="text-lg font-bold">
+                      {product.product.name}
+                    </h3>
+                    <p className="text-blue-800">{product.product.jenis}</p>
+                    <p className="text-red-600 font-bold">
+                      {formatRp(product.product.price)}
+                    </p>
+                  </div>
+                </div>
+                <div className="flex flex-col justify-center items-end gap-4">
+                  <div className="flex items-center gap-6">
+                    <Button
+                      type={"button"}
+                      icon={<IoMdAdd />}
+                      onClick={() => {
+                        cartContext.cart[idx].qty++;
+                        cartContext.setCart([...cartContext.cart]);
+                      }}
+                    />
+                    <p className="text-gray-800 font-medium">{product.qty}</p>
+                    <Button
+                      type={"button"}
+                      icon={<IoMdRemove />}
+                      color="red"
+                      onClick={() => {
+                        if (cartContext.cart[idx].qty <= 1) {
+                          cartContext.cart.splice(idx, 1);
+                          cartContext.setCart([...cartContext.cart]);
+                        } else {
+                          cartContext.cart[idx].qty--;
+                          cartContext.setCart([...cartContext.cart]);
+                        }
+                      }}
+                    />
+                  </div>
+                  <p className="text-blue-900 font-bold text-xl">
+                    {formatRp(product.product.price * product.qty)}
                   </p>
                 </div>
               </div>
-              <div className="flex flex-col justify-center items-end gap-4">
-                <div className="flex items-center gap-6">
-                  <Button
-                    type={"button"}
-                    icon={<IoMdAdd />}
-                    onClick={() => {
-                      cartContext.cart[idx].qty++;
-                      cartContext.setCart([...cartContext.cart]);
-                    }}
-                  />
-                  <p className="text-gray-800 font-medium">{product.qty}</p>
-                  <Button
-                    type={"button"}
-                    icon={<IoMdRemove />}
-                    color="red"
-                    onClick={() => {
-                      if (cartContext.cart[idx].qty <= 1) {
-                        cartContext.cart.splice(idx, 1);
-                        cartContext.setCart([...cartContext.cart]);
-                      } else {
-                        cartContext.cart[idx].qty--;
-                        cartContext.setCart([...cartContext.cart]);
-                      }
-                    }}
-                  />
-                </div>
-                <p className="text-blue-900 font-bold text-xl">
-                  {formatRp(product.product.price * product.qty)}
-                </p>
+            );
+          })}
+          <div className="w-full bg-white shadow-lg rounded-lg p-3">
+            <div className="flex justify-between mb-6">
+              <h3 className="text-lg font-bold">Total Order</h3>
+              <p className="text-blue-950 text-2xl font-bold">
+                {formatRp(
+                  cartContext!.cart.reduce(
+                    (sum, item) => sum + item.qty * item.product.price,
+                    0
+                  )
+                )}
+              </p>
+            </div>
+            <form>
+              <Textfield required label={"Shipping Address"} />
+              <div className="flex justify-center">
+                <Button type={"submit"} text="Order Now" />
               </div>
-            </div>
-          );
-        })}
-        <div className="w-full bg-white shadow-lg rounded-lg p-3">
-          <div className="flex justify-between mb-6">
-            <h3 className="text-lg font-bold">Total Order</h3>
-            <p className="text-blue-950 text-2xl font-bold">
-              {formatRp(
-                cartContext!.cart.reduce(
-                  (sum, item) => sum + item.qty * item.product.price,
-                  0
-                )
-              )}
-            </p>
+            </form>
           </div>
-          <form>
-            <Textfield required label={"Shipping Address"} />
-            <div className="flex justify-center">
-              <Button type={"submit"} text="Order Now" />
-            </div>
-          </form>
         </div>
-      </div>
+      ) : (
+        <>
+          <div className="flex flex-col items-center mt-20 gap-6">
+            <div className="text-xl text-third">
+              Your Cart is Empty! Let's Shopping
+            </div>
+            <Button
+              type={"button"}
+              text="Shop Now"
+              onClick={() => navigator("/list-product")}
+            />
+          </div>
+        </>
+      )}
     </div>
   );
 }
